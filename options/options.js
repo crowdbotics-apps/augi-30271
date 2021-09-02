8f53c8fb47025e7c9d8a84776c41aa76b9b3e2c9
Crowdbotics <team@crowdbotics.com>
2021-09-02 10:48:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "augi_30271Identifier",
+  url: "https://augi_30271Identifier.botics.co",
+  api: "https://augi_30271Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
